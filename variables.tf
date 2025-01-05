554ea1cfde50554df88288c5d2eb94490dbfb655
U-Myo-Min <myomin1986@gmail.com>
2025-01-05 13:33:50 +0630

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "username" {
+  default = "admin"
+}
+variable "password" {
+}
+variable "controller_ip" {
+}
+variable "ace_password" {
+}
+variable "aws_account_name" {
+default = "aws-account"
+}
+variable "aws_transit_instance_size" {
+default = "t2.micro"
+}
